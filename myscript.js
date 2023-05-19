f86eb0c1dd247c523699c61dbd065f0b092aef5a
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-19 16:46:26 +0200

    prova

---
+++ myscript.js
@@ -0,0 +1,13 @@
+const array[];
+array.push(n1);
+let i = 0;
+let superato30=false;
+while ( superato30===false) {
+    const n = parseInt(prompt("Type a number"));
+    array.push(n);
+    }const somma += array[i];{
+    if( somma > 50 ){
+        superato30=true;
+    }
+    i++;
+}
